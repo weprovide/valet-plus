1b857bd44ea24436f7cbd8f115aaa70d719a60f6
Tim Neutkens <timneutkens@me.com>
2017-07-08 22:29:50 +0200

    Fix Magento2 setup page

--- cli/drivers/Magento2ValetDriver.php
+++ cli/drivers/Magento2ValetDriver.php
@@ -3,74 +3,96 @@
 class Magento2ValetDriver extends ValetDriver
 {
 
-/**
- * Determine if the driver serves the request.
- *
- * @param  string  $sitePath
- * @param  string  $siteName
- * @param  string  $uri
- * @return bool
- */
-public function serves($sitePath, $siteName, $uri)
-{
-    return file_exists($sitePath.'/pub/index.php') &&
-    file_exists($sitePath.'/bin/magento');
-}
-
-/**
- * Determine if the incoming request is for a static file.
- *
- * @param  string  $sitePath
- * @param  string  $siteName
- * @param  string  $uri
- * @return string|false
- */
-public function isStaticFile($sitePath, $siteName, $uri)
-{
-    $isMagentoStatic = false;
-    $resource = $uri;
-
-    if(strpos($uri, '/static/') !== false) {
-        $isMagentoStatic = true;
+    /**
+     * Determine if the driver serves the request.
+     *
+     * @param  string $sitePath
+     * @param  string $siteName
+     * @param  string $uri
+     * @return bool
+     */
+    public function serves($sitePath, $siteName, $uri)
+    {
+        return file_exists($sitePath . '/pub/index.php') &&
+            file_exists($sitePath . '/bin/magento');
     }
 
-    if(!$isMagentoStatic && strpos($uri, '/media/') === false) {
+    /**
+     * Determine if the incoming request is for a static file.
+     *
+     * @param  string $sitePath
+     * @param  string $siteName
+     * @param  string $uri
+     * @return string|false
+     */
+    public function isStaticFile($sitePath, $siteName, $uri)
+    {
+        $isMagentoStatic = false;
+        $resource = $uri;
+
+        if(strpos($uri,'/pub') === 0 && file_exists($sitePath.'/setup'.$uri)) {
+            return $sitePath.'/setup'.$uri;
+        }
+
+        if (strpos($uri, '/static/') !== false) {
+            $isMagentoStatic = true;
+        }
+
+        if (!$isMagentoStatic && strpos($uri, '/media/') === false) {
+            return false;
+        }
+
+        if ($isMagentoStatic) {
+            $resource = preg_replace('#static(/version[0-9]+)?/#', '', $uri, 1);
+            $uri = '/static' . $resource;
+        }
+
+        if (file_exists($staticFilePath = $sitePath . '/pub' . $uri)) {
+            return $staticFilePath;
+        }
+
+        if (strpos($uri, '/static/') === 0) {
+            $_GET['resource'] = $resource;
+            include($sitePath . DIRECTORY_SEPARATOR . 'pub' . DIRECTORY_SEPARATOR . 'static.php');
+            exit;
+        }
+
+        if (strpos($uri, '/media/') === 0) {
+            include($sitePath . DIRECTORY_SEPARATOR . 'pub' . DIRECTORY_SEPARATOR . 'get.php');
+            exit;
+        }
+
         return false;
     }
 
-    if($isMagentoStatic) {
-        $resource = preg_replace('#static(/version[0-9]+)?/#', '', $uri, 1);
-        $uri = '/static' . $resource;
+    /**
+     * Get the fully resolved path to the application's front controller.
+     *
+     * @param  string $sitePath
+     * @param  string $siteName
+     * @param  string $uri
+     * @return string
+     */
+    public function frontControllerPath($sitePath, $siteName, $uri)
+    {
+        if($uri === '/setup') {
+            Header('HTTP/1.1 301 Moved Permanently');
+            Header('Location: http://' . $_SERVER['HTTP_HOST'] . $uri . '/');
+            die;
+        }
+
+        if(strpos($uri, '/setup') === 0) {
+            $_SERVER['SCRIPT_FILENAME'] = $sitePath.'/setup/index.php';
+            $_SERVER['SCRIPT_NAME'] = '/index.php';
+            $_SERVER['DOCUMENT_ROOT'] = $sitePath.'/setup/';
+            $_SERVER['REQUEST_URI'] = str_replace('/setup', '', $_SERVER['REQUEST_URI']);
+
+            if($_SERVER['REQUEST_URI'] === '') {
+                $_SERVER['REQUEST_URI'] = '/';
+            }
+            return $sitePath.'/setup/index.php';
+        }
+
+        return $sitePath . '/pub/index.php';
     }
-
-    if (file_exists($staticFilePath = $sitePath.'/pub'.$uri)) {
-        return $staticFilePath;
-    }
-
-    if (strpos($uri, '/static/') === 0) {
-        $_GET['resource'] = $resource;
-        include($sitePath.DIRECTORY_SEPARATOR.'pub'.DIRECTORY_SEPARATOR.'static.php');
-        exit;
-    }
-
-    if (strpos($uri, '/media/') === 0) {
-        include($sitePath.DIRECTORY_SEPARATOR.'pub'.DIRECTORY_SEPARATOR.'get.php');
-        exit;
-    }
-
-    return false;
-}
-
-/**
- * Get the fully resolved path to the application's front controller.
- *
- * @param  string  $sitePath
- * @param  string  $siteName
- * @param  string  $uri
- * @return string
- */
-public function frontControllerPath($sitePath, $siteName, $uri)
-{
-    return $sitePath.'/pub/index.php';
-}
 }
