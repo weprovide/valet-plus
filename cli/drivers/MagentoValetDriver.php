1cc480983b749b8404a4213e239a5325fa0fdef8
Tim Neutkens <timneutkens@me.com>
2017-07-19 22:32:23 +0200

    Add valet configure for Magento 1

---
+++ cli/drivers/MagentoValetDriver.php
@@ -0,0 +1,44 @@
+<?php
+
+class MagentoValetDriver extends BasicValetDriver
+{
+    /**
+     * Determine if the driver serves the request.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return bool
+     */
+    public function serves($sitePath, $siteName, $uri)
+    {
+        return is_dir($sitePath.'/app/code/core/Mage');
+    }
+
+    public function configure($devtools, $url) {
+        info('Configuring Magento...');
+
+        $sitePath = getcwd();
+
+        if(!file_exists($sitePath.'/app/etc/local.xml')) {
+            info('local.xml missing. Installing default local.xml...');
+            $devtools->files->putAsUser(
+                $sitePath.'/app/etc/local.xml',
+                str_replace(
+                    'DBNAME',
+                    $devtools->mysql->getDirName(),
+                    $devtools->files->get(__DIR__.'/../stubs/magento/local.xml')
+                )
+            );
+        }
+
+        info('Setting base url...');
+        $devtools->cli->quietlyAsUser('n98-magerun config:set web/unsecure/base_url ' . $url . '/');
+        $devtools->cli->quietlyAsUser('n98-magerun config:set web/secure/base_url ' . $url . '/');
+
+        info('Flushing cache...');
+        $devtools->cli->quietlyAsUser('n98-magerun cache:flush');
+
+        info('Configured Magento');
+    }
+}
