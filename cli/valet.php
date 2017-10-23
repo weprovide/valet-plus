a6fb01516c4ac87f9c3e64d26badac8cad053035
Tim Neutkens <timneutkens@me.com>
2017-10-23 14:48:51 +0200

    1.0.9

--- cli/valet.php
+++ cli/valet.php
@@ -19,7 +19,7 @@ use Symfony\Component\Console\Question\ConfirmationQuestion;
  */
 Container::setInstance(new Container);
 
-$version = '1.0.8';
+$version = '1.0.9';
 
 $app = new Application('Valet+', $version);
 
