9b3c817cc57ea85b4b37594e92f06fcf834a3835
Johann Zelger <j.zelger@techdivision.com>
2018-04-10 09:19:17 +0200

    reinstall composer instead of install

--- cli/scripts/fix-homebrew-php.sh
+++ cli/scripts/fix-homebrew-php.sh
@@ -8,7 +8,7 @@ brew untap homebrew/php
 brew install php@7.0
 brew link php@7.0 --overwrite --force
 sudo ln -sf /usr/local/bin/php* /usr/bin/
-HOMEBREW_NO_ENV_FILTERING=1 brew install composer
+HOMEBREW_NO_ENV_FILTERING=1 brew reinstall composer
 composer global require techdivision/valet-plus
 grep -q 'export PATH="$PATH:$HOME/.composer/vendor/bin"' ~/.bashrc || echo 'export PATH="$PATH:$HOME/.composer/vendor/bin"' >> ~/.bashrc
 grep -q 'export PATH="$PATH:$HOME/.composer/vendor/bin"' ~/.zshrc || echo 'export PATH="$PATH:$HOME/.composer/vendor/bin"' >> ~/.zshrc
