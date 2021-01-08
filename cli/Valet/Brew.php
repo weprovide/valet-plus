1c5b0b38fc4583e1379298d7d150d335fb0c933c
Peter Keijsers <p.j.keijsers@gmail.com>
2021-01-08 14:44:23 +0100

    Fix for Brew 2.7.0 brew list command without arguments is deprecated (#554)
    This issues caused a complete malfunction of Valet plus after updating my brew to version: 2.7.0
    
    This issues prevents Valet plus from starting services and installing packages.

--- cli/Valet/Brew.php
+++ cli/Valet/Brew.php
@@ -30,7 +30,7 @@ class Brew
      */
     public function installed($formula)
     {
-        return in_array($formula, explode(PHP_EOL, $this->cli->runAsUser('brew list | grep ' . $formula)));
+        return in_array($formula, explode(PHP_EOL, $this->cli->runAsUser('brew list --formula | grep ' . $formula)));
     }
 
     /**
