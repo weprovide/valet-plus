b8d0b854e4c79c657d0b24fd37e62b1fdbfec0ad
Mischa Braam <mischa@weprovide.com>
2021-01-24 10:44:01 +0100

    Fix missing fpm config path for php8.0

--- cli/Valet/PhpFpm.php
+++ cli/Valet/PhpFpm.php
@@ -123,6 +123,7 @@ class PhpFpm
     public function fpmConfigPath()
     {
         $confLookup = [
+            self::PHP_V80_VERSION => self::LOCAL_PHP_FOLDER . '8.0/php-fpm.d/www.conf',
             self::PHP_V74_VERSION => self::LOCAL_PHP_FOLDER . '7.4/php-fpm.d/www.conf',
             self::PHP_V73_VERSION => self::LOCAL_PHP_FOLDER . '7.3/php-fpm.d/www.conf',
             self::PHP_V72_VERSION => self::LOCAL_PHP_FOLDER . '7.2/php-fpm.d/www.conf',
