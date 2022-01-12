e12bc2a31f2307d83f5a6dd83e7e3d17c65ff332
Oscar Recio <osrecio@gmail.com>
2022-01-12 13:00:47 +0100

    Use facade instead of call new Class

--- cli/valet.php
+++ cli/valet.php
@@ -940,14 +940,13 @@ if (is_dir(VALET_HOME_PATH)) {
     })->descriptions('Display all of the registered Valet rewrites');
 
     $app->command('logs [service]', function ($service) {
-        $arquitecture = new \Valet\Architecture(new \Valet\CommandLine());
         $logs = [
             'php' => '$HOME/.valet/Log/php.log',
-            'php-fpm' => $arquitecture->getBrewPath() . '/var/log/php-fpm.log',
+            'php-fpm' => Architecture::getBrewPath() . '/var/log/php-fpm.log',
             'nginx' => '$HOME/.valet/Log/nginx-error.log',
             'mysql' => '$HOME/.valet/Log/mysql.log',
             'mailhog' => $arquitecture->getBrewPath() . '/var/log/mailhog.log',
-            'redis' => $arquitecture->getBrewPath() . '/var/log/redis.log',
+            'redis' => Architecture::getBrewPath() . '/var/log/redis.log',
         ];
 
         if (!isset($logs[$service])) {
